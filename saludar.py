bb274685bcac3a07d6980ece4ba559ff2d91a2c9
carlospuigserver <cpuiggut@myuax.com>
2022-12-09 16:16:38 +0100

    commit

--- saludar.py
+++ saludar.py
@@ -1,2 +1,11 @@
 def saludar():
     print("Hola mundo")
+
+
+class Saludo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def Saludar(self):
+          print("Hola mundo",self.nombre)
+
