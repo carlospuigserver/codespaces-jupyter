f37c2fad6a301049840de515ab3ccdfb7402e09f
carlospuigserver <cpuiggut@myuax.com>
2022-12-09 16:38:33 +0100

    commit

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from saludar import Saludo
-
+from test.hola .aludar import Saludo
+from test.despedidas.saludar import Saludo
 
 saludo=Saludo('Juan')
 saludo.saludo()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from intertool import setup
+
+setup(
+    name='paquete',
+    version='0.1',
+    description='Paquete de prueba',
+    author='Carlos',
+    package='paquete',
+    packages=['paquete', 'paquete.hola','paquete.despedidas'],
+    sripts=['']
+)
